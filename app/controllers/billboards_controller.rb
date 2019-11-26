ba2ec7e2c9e6d9025ce7b60fac4f9abebaf9cc6d
Aaron Cookson <cookson.aaron@gmail.com>
2019-11-25 21:33:50 -0700

    Billboards model and controller

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,30 +1,29 @@
 class BillboardsController < ApplicationController
   
+  before_action :set_billboard, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @billboards = Billboard.all
   end
 
   def show
-    set_billboard
   end
   
+  def new
+    @billboard = Billboard.new
+  end
+
   def create
-    @billboard = Billboard.new(params[billboard_params])
+    @billboard = Billboard.new(billboard_params)
     if @billboard.save
       redirect_to @billboard
     else
       render :new
     end
   end
-  
-  def new
-    @billboard = Billboard.new
-    # render partial: 'form'
-  end
 
   def edit
-    # render partial: 'form'
   end
 
   def destroy
