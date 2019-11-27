2315620a71f894d42d2b497b38b8f9b43e643148
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2019-11-26 19:43:37 -0700

    Fix typos

--- app/models/billboard.rb
+++ app/models/billboard.rb
@@ -2,7 +2,6 @@ class Billboard < ApplicationRecord
     validates :name, uniqueness: true
     validates :name, presence: true 
     
-    
-
-    # has_many artists 
+    has_many :artists 
+    has_many :songs, through: :artists
 end
