bf848a173bfed562fff62c3bc19ebc9a78860404
Gabriella Algecera <gabriellaalgecera@Brians-MacBook-Pro.local>
2019-11-26 19:58:15 -0700

    artists songs controllers added

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,41 @@
 class ArtistsController < ApplicationController
-  def index
-  end
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
-  def new
+  def index
+    @artists = Artist.all
   end
 
   def show
   end
+  
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist
+    else
+      render :new
+    end
+  end
 
   def edit
   end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_path(@artist)
+  end 
+
+  private 
+
+     def artist_params
+        params.require(:artist.permit(:name, :rank, :genre)
+     end 
+
+     def set_artist
+      @artist = Artist.find(params[:id])
+    end
 end
