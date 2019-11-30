d5533b139858843771323a4ea2679651e455a53b
Aaron Cookson <cookson.aaron@gmail.com>
2019-11-30 14:40:05 -0700

    fixed seed for songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@
         genre: Faker::Music.genre,
       )
     end
-
     10.times do
-      Artist.create( 
+      artist = Artist.create( 
         name: Faker::Music.band,
         genre: Faker::Music.genre,
       )
-    end
-
-    20.times do
-      Song.create( 
-        name: Faker::Book.title,
-        genre: Faker::Music.genre,
-      )
+      20.times do
+        Song.create( 
+          name: Faker::Book.title,
+          genre: Faker::Music.genre,
+          artist_id: artist.id
+          # billboard_id: billboards.sample.id
+        )
+      end
     end
 
     puts "Finished Seeding"
